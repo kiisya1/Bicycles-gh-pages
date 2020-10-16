28522e296a372733686560972e684f42f7089b80
Anna Pronina <kiisya1@yandex.ru>
2020-10-16 11:20:51 +0300

    Вносит изменения в блок видео

--- js/script.js
+++ js/script.js
@@ -112,7 +112,8 @@
     function init() {
       var myMap = new ymaps.Map('map', {
         center: [59.938635, 30.323118],
-        zoom: 16
+        zoom: 16,
+        controls: ['smallMapDefaultSet']
       });
     }
   }
