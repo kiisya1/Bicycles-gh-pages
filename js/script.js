044c9ae244447847f903038c2ff74b4101a58051
Anna Pronina <kiisya1@yandex.ru>
2020-10-16 17:06:13 +0300

    Вносит правки

--- js/script.js
+++ js/script.js
@@ -5,8 +5,7 @@
   var body = document.querySelector('body');
   var header = document.querySelector('.header');
   var logo = header.querySelector('.header__logo');
-  var openButton = header.querySelector('.header__menu-toggle--open');
-  var closeButton = header.querySelector('.header__menu-toggle--close');
+  var menuButton = header.querySelector('.header__menu-toggle');
   var nav = header.querySelector('.header__main-nav');
   var links = nav.querySelectorAll('.main-nav__link');
 
@@ -20,24 +19,17 @@
   // Закрываем меню
 
   var closeMenu = function () {
-    if (closeButton.classList.contains('header__menu-toggle--active')) {
-      body.classList.remove('body-lock');
-      closeButton.classList.remove('header__menu-toggle--active');
-      openButton.classList.add('header__menu-toggle--active');
-      nav.classList.remove('main-nav--opened');
-    }
+    body.classList.remove('body-lock');
+    menuButton.classList.remove('header__menu-toggle--active');
+    nav.classList.remove('main-nav--opened');
   };
 
   // Открываем меню
 
   var openMenu = function () {
-    if (openButton.classList.contains('header__menu-toggle--active')) {
-      body.classList.add('body-lock');
-      openButton.classList.remove('header__menu-toggle--active');
-      closeButton.classList.add('header__menu-toggle--active');
-      logo.classList.add('logo--menu-opened');
-      nav.classList.add('main-nav--opened');
-    }
+    body.classList.add('body-lock');
+    menuButton.classList.add('header__menu-toggle--active');
+    nav.classList.add('main-nav--opened');
   };
 
   // Добавляем обработчик собития на ссылку
@@ -62,20 +54,15 @@
     }
   }
 
-  if (openButton) {
-    openButton.classList.remove('header__menu-toggle--no-js');
-    openButton.classList.add('header__menu-toggle--active');
-    openButton.addEventListener('click', function (evt) {
+  if (menuButton) {
+    menuButton.classList.remove('header__menu-toggle--no-js');
+    menuButton.addEventListener('click', function (evt) {
       evt.preventDefault();
-      openMenu();
-    });
-  }
-
-  if (closeButton) {
-    closeButton.classList.remove('header__menu-toggle--no-js');
-    closeButton.addEventListener('click', function (evt) {
-      evt.preventDefault();
-      closeMenu();
+      if (menuButton.classList.contains('header__menu-toggle--active')) {
+        closeMenu();
+      } else {
+        openMenu();
+      }
     });
   }
 
